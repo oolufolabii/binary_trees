070e56a66696e209ea61d8cac2bec0576c60642d
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-20 10:26:54 +0100

    Binary Tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_delete - deletes an entire binary tree
- * 
+ *
  * @tree: pointer to the root node of the tree to delete
  */
 
@@ -10,7 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-	
+
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);

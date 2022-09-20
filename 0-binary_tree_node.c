56a5d167200eb29b7a6b0cb3b5deac0af5bfb7ed
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-20 09:04:53 +0100

    Binary Tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,5 +20,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->right, new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->left = NULL;
 	return (new_node);

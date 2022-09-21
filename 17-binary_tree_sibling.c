db9498eb3c9188b3a1efcd286480c698bc1a54d7
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-21 10:48:00 +0100

    Binary Tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,11 +2,17 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node
- * 
+ *
  * @node: a pointer to the node to find the sibling
  * Return: pointer to sibling node, otherwise NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (!node || !node->parent)
+		return (NULL);
 
+	if (node == node->parent->left)
+		return (node->parent->right);
+
+	return (node->parent->left);
 }

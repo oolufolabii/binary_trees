4c96c099d6d02c6d5f55af9e3a155899545877c6
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-21 21:37:59 +0100

    Binary Tree -Advanced Tasks

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -59,7 +59,7 @@ void binary_tree_postorder(const binary_tree_t *tree,
 
 /**
  * binary_tree_levelorder - goes through a binary
- * 		tree using level-order traversal
+ * 				tree using level-order traversal
  *
  * @tree: pointer to the root node of the tree to traverse
  * @func:  pointer to a function to call for each node.

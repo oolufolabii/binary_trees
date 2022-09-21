76bde3eb135461c10f8d354e5715aabfd8fd8c59
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-21 12:29:01 +0100

    Binary Tree -Advanced Tasks

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -36,13 +36,26 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (!first || !second)
 		return (NULL);
-	
+
 	first_depth = binary_tree_depth(first);
 	second_depth = binary_tree_depth(second);
 
 	while (first_depth > second_depth)
+	{
 		first = first->parent;
 		first_depth--;
-		
-	
+	}
+	while (second_depth > first_depth)
+	{
+		second = second->parent;
+		second_depth--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
 }

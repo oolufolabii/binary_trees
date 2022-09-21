924ae0338342872b30ed7d03bf99191f3d54cb97
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-21 12:12:03 +0100

    Binary Tree

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: pointer to the node to measure the depth
+ * Return: returns the depth of the node, otherwise null if empty node.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t tree_depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		tree_depth++;
+		tree = tree->parent;
+	}
+	return (tree_depth);
+}
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ *
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: common ancestor, otherwise NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
+{
+	size_t first_depth, second_depth;
+
+	if (!first || !second)
+		return (NULL);
+	
+	first_depth = binary_tree_depth(first);
+	second_depth = binary_tree_depth(second);
+
+	while (first_depth > second_depth)
+		first = first->parent;
+		first_depth--;
+		
+	
+}
--- binary_trees.h
+++ binary_trees.h
@@ -32,6 +32,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/* Basic Tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -51,5 +52,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/* Advanced Tasks */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 #endif

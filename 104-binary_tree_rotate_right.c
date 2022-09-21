7658702abe1865e52a849e288add5b51a75ffa57
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-21 21:33:41 +0100

    Binary Tree -Advanced Tasks

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
+ * binary_tree_rotate_right - - performs a right-rotation on a binary tree
  * 
- * @param tree 
- * @return binary_tree_t* 
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: pointer to the new root node of the tree once rotated 
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
